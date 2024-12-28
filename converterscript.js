59f0af9d8452293f3eef773dba9aa1e27547eb87
Shivika-5236 <shivikaomprakash62@gmail.com>
2024-12-28 14:10:59 +0530

    Update converterscript.js

--- converterscript.js
+++ converterscript.js
@@ -45,7 +45,7 @@ const numberToWordsIndian = (num) => {
     const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    const scales = ['', 'thousand', 'lakh', 'crore'];
+    const scales = ['', 'thousand', 'lakh', 'crore','arab', 'kharab', 'neel', 'padma', 'shankh'];
 
     if (num === 0) return 'zero';
 
